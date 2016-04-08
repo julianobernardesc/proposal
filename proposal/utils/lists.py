a2c2d2e6677d257e4c4cf5f5b4281c5a87aa7954
julianobernardesc <bernardes.engenharia@gmail.com>
2016-04-07 21:20:10 -0300

    Utils

---
+++ proposal/utils/lists.py
@@ -0,0 +1,13 @@
+PHONE_TYPE = (
+    ('pri', 'principal'),
+    ('com', 'comercial'),
+    ('res', 'residencial'),
+    ('cel', 'celular'),
+    ('cl', 'Claro'),
+    ('oi', 'Oi'),
+    ('t', 'Tim'),
+    ('v', 'Vivo'),
+    ('n', 'Nextel'),
+    ('fax', 'fax'),
+    ('o', 'outros'),
+)
